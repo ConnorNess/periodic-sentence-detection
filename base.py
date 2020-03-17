9f383b6a24e447c55fec499c8102363d2ab8240f
Connor Ness <Connorrness@Gmail,com>
2020-03-17 08:37:43 +0000

    bleehh

--- base.py
+++ base.py
@@ -2,8 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-#PunktSentenceTokenizer #Punkt is trained but can be retrained on data, going to use for ML later on
+from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 #nltk.download()
@@ -21,22 +20,33 @@ if os.path.exists(os.path.join('TrainingCleaned.txt')): #Deleting and remaking c
 cleantrain_file = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'w') 
 
 sentenceTraining = nltk.sent_tokenize(train_file)
-train_pos = [] #Part-of-Speech base check build from training
-stemmed_train = []
+punctuationremover = RegexpTokenizer(r'\w+')
 
+part_of_speech_trends = []
+#TODO add tokenizer which includes punctuation, could also be a trend
 for sentence in sentenceTraining:
-    words = nltk.word_tokenize(sentence)
-    filtered_train = [w for w in words if not w in stop_words] #Adds token words provided they are not stop words
-    
-    for word in filtered_train: #stems each word in sentence and writes each sentence to a new line
+    sentencePoS = ""
+    wordCounter = 0
+
+    words = punctuationremover.tokenize(sentence)
+    filtered_trainer = [w for w in words if not w in stop_words] #Takes out punctuation then stop words from each sentence
+    tagged_sentence_trainer = nltk.pos_tag(filtered_trainer)
+
+    for word in filtered_trainer: #stems each word in sentence and writes each sentence to a new line
         cleantrain_file.write(ps.stem(word))
         cleantrain_file.write(" ")
+        sentencePoS += (tagged_sentence_trainer[wordCounter][1])
+        sentencePoS += " "
+        wordCounter += 1
     cleantrain_file.write("\n")
+    
+    cleantrain_file.write(sentencePoS)
+    cleantrain_file.write("\n")
+    
 cleantrain_file.close()
 
-# training_PoS_Tags = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'r')
-# train_PoS_frequency = []
-
+training_PoS_Tags = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'r')
+train_PoS_frequency = []
 
 
 # for line in training_PoS_Tags:
@@ -50,7 +60,9 @@ cleantrain_file.close()
 #             train_PoS_frequency = [nltk.pos_tag(word)]
 #     print(train_PoS_frequency)
 
-training_tag_rate = nltk.FreqDist(tag for (word, tag) in )
+#training_tag_rate = nltk.FreqDist(tag for (word, tag) in )
+
+
 
 ###---------------------------------------------------INPUT---------------------------------------------------###
 def input_file(): #Takes in input file and tokenizes sentences
