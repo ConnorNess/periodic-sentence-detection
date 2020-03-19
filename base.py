410c88d7155a23c165efbd6ce2c5e8238fb49be8
Connor Ness <Connorrness@Gmail,com>
2020-03-19 15:51:22 +0000

    spageti

--- base.py
+++ base.py
@@ -22,47 +22,39 @@ cleantrain_file = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'w')
 sentenceTraining = nltk.sent_tokenize(train_file)
 punctuationremover = RegexpTokenizer(r'\w+')
 
-part_of_speech_trends = []
-#TODO add tokenizer which includes punctuation, could also be a trend
-for sentence in sentenceTraining:
-    sentencePoS = ""
-    wordCounter = 0
+part_of_speech_trends = [] #TODO: Numpy array for both PoS inputs for count occurances
 
-    words = punctuationremover.tokenize(sentence)
-    filtered_trainer = [w for w in words if not w in stop_words] #Takes out punctuation then stop words from each sentence
+for sentence in sentenceTraining:
+    word_counter = 0
+    sentencePoS = ""
+    sentencePoS_no_punctuation = ""
+
+    words = word_tokenize(sentence)
+    filtered_trainer = [w for w in words if not w in stop_words] #cleaning stop words
     tagged_sentence_trainer = nltk.pos_tag(filtered_trainer)
 
-    for word in filtered_trainer: #stems each word in sentence and writes each sentence to a new line
+    for word in filtered_trainer:
         cleantrain_file.write(ps.stem(word))
         cleantrain_file.write(" ")
-        sentencePoS += (tagged_sentence_trainer[wordCounter][1])
+
+        sentencePoS += (tagged_sentence_trainer[word_counter][1])
         sentencePoS += " "
-        wordCounter += 1
+
+        if punctuationremover.tokenize(word):
+            sentencePoS_no_punctuation += (tagged_sentence_trainer[word_counter][1])
+            sentencePoS_no_punctuation += " "
+
+        word_counter += 1
     cleantrain_file.write("\n")
-    
     cleantrain_file.write(sentencePoS)
+    cleantrain_file.write("\n")
+    cleantrain_file.write(sentencePoS_no_punctuation)
     cleantrain_file.write("\n")
     
 cleantrain_file.close()
 
 training_PoS_Tags = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'r')
 train_PoS_frequency = []
-
-
-# for line in training_PoS_Tags:
-#     word_count = len(line.split())
-#     print(word_count)
-#     current_word = 1
-#     main_clause_position = round(((len(line.split())/100)*31.4))
-    
-#     for word in line.split():
-#         if current_word >  main_clause_position:
-#             train_PoS_frequency = [nltk.pos_tag(word)]
-#     print(train_PoS_frequency)
-
-#training_tag_rate = nltk.FreqDist(tag for (word, tag) in )
-
-
 
 ###---------------------------------------------------INPUT---------------------------------------------------###
 def input_file(): #Takes in input file and tokenizes sentences
