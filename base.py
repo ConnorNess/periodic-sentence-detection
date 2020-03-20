5cc6527c4b4dca4cb7552e6c3ea264dd27206795
Connor Ness <Connorrness@Gmail,com>
2020-03-20 10:46:55 +0000

    check like that comma do be

--- base.py
+++ base.py
@@ -6,12 +6,13 @@ from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 #nltk.download()
+import numpy
 import os
 
 stop_words = set(stopwords.words('english'))
 ps = PorterStemmer()
 
-###---------------------------------------------------TRAINING---------------------------------------------------###
+###---------------------------------------------------CLEANING---------------------------------------------------###
 training_file = open(os.path.join(sys.path[0],'TrainingBase.txt'))
 train_file = training_file.read()
 
@@ -22,8 +23,6 @@ cleantrain_file = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'w')
 sentenceTraining = nltk.sent_tokenize(train_file)
 punctuationremover = RegexpTokenizer(r'\w+')
 
-part_of_speech_trends = [] #TODO: Numpy array for both PoS inputs for count occurances
-
 for sentence in sentenceTraining:
     word_counter = 0
     sentencePoS = ""
@@ -32,6 +31,7 @@ for sentence in sentenceTraining:
     words = word_tokenize(sentence)
     filtered_trainer = [w for w in words if not w in stop_words] #cleaning stop words
     tagged_sentence_trainer = nltk.pos_tag(filtered_trainer)
+    cleantrain_file.write("@: ") #Marks source sentence
 
     for word in filtered_trainer:
         cleantrain_file.write(ps.stem(word))
@@ -46,15 +46,49 @@ for sentence in sentenceTraining:
 
         word_counter += 1
     cleantrain_file.write("\n")
-    cleantrain_file.write(sentencePoS)
+    cleantrain_file.write("#: " + sentencePoS) #Marks punctuated parts of speech
     cleantrain_file.write("\n")
-    cleantrain_file.write(sentencePoS_no_punctuation)
+    cleantrain_file.write("%: " + sentencePoS_no_punctuation) #Marks non-punctuated parts of speech
     cleantrain_file.write("\n")
     
 cleantrain_file.close()
 
-training_PoS_Tags = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'r')
-train_PoS_frequency = []
+###---------------------------------------------------TRAINING---------------------------------------------------###
+training_PoS_file = open(os.path.join(sys.path[0],'TrainingCleaned.txt'), 'r')
+PoS_frequency = []
+#PoS_frequency_no_punctuation = numpy.array([])
+comma_length_to_end = []
+
+
+for line in training_PoS_file:
+    words_after_comma = 0
+    comma_present = False
+
+    if line[0:3] == "#: ":
+        trimmed_line = line[3:-3] #Takes off identifier and newline straggler
+        PoS_frequency.append(trimmed_line)
+        for word in line:
+            if word == "," and comma_present == False:
+                comma_present = True
+            if word == "," and comma_present == True:
+                words_after_comma = 0
+            if word != "," and comma_present == True:
+                words_after_comma += 1
+        
+        if words_after_comma != 0:
+            comma_length_to_end.append(words_after_comma)
+
+    #if line[0:3] == "%: ":
+        #trimmed_line = line[3:-3]
+        #numpy.append(PoS_frequency_no_punctuation, line)
+
+(unique, counts) = numpy.unique(PoS_frequency, return_counts=True)
+frequencies = numpy.asarray((unique, counts)).T
+#print(frequencies)
+
+(unique, counts) = numpy.unique(comma_length_to_end, return_counts=True)
+frequencies = numpy.asarray((unique, counts)).T
+print(frequencies)
 
 ###---------------------------------------------------INPUT---------------------------------------------------###
 def input_file(): #Takes in input file and tokenizes sentences
